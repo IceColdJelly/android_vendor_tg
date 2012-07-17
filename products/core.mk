f6df73c5ad3c4477a6c63c5126c3efbfb9934682
Tom Gascoigne <tom@gascoigne.me>
2012-07-17 12:44:10 +0100

    Add prebuilt Superuser.apk

--- products/core.mk
+++ products/core.mk
@@ -10,4 +10,5 @@ PRODUCT_COPY_FILES += \
 	$(VENDOR_PATH)/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
 	$(VENDOR_PATH)/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
 	$(VENDOR_PATH)/prebuilt/etc/apns-conf-cdma.xml:system/etc/apns-conf-cdma.xml \
-	$(VENDOR_PATH)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml
+	$(VENDOR_PATH)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml \
+	$(VENDOR_PATH)/prebuilt/app/Superuser.apk:system/app/Superuser.apk
