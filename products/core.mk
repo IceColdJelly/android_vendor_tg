45e79c83e89ddbf3b510ea5c6eaa67932b6385e3
Tom Gascoigne <tom@gascoigne.me>
2012-07-18 10:12:33 +0100

    Added ChainsDD's superuser

--- products/core.mk
+++ products/core.mk
@@ -4,11 +4,12 @@ VENDOR_PATH := vendor/tg
 DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-	Camera
+	Camera \
+	Superuser \
+	su
 
 PRODUCT_COPY_FILES += \
 	$(VENDOR_PATH)/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
 	$(VENDOR_PATH)/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
 	$(VENDOR_PATH)/prebuilt/etc/apns-conf-cdma.xml:system/etc/apns-conf-cdma.xml \
 	$(VENDOR_PATH)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml \
-	$(VENDOR_PATH)/prebuilt/app/Superuser.apk:system/app/Superuser.apk
