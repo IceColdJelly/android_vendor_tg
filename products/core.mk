e5f7d24c0071b7a5f9c614cec3b83478157d9fa7
LorD ClockaN <davor@losinj.com>
2012-07-23 21:34:54 +0200

    use prebuilt su and SuperSU

--- products/core.mk
+++ products/core.mk
@@ -4,12 +4,15 @@ VENDOR_PATH := vendor/tg
 DEVICE_PACKAGE_OVERLAYS += $(VENDOR_PATH)/overlay
 
 PRODUCT_PACKAGES += \
-	Camera \
-	Superuser \
-	su
+	Camera
 
 PRODUCT_COPY_FILES += \
 	$(VENDOR_PATH)/prebuilt/media/bootanimation.zip:system/media/bootanimation.zip \
 	$(VENDOR_PATH)/prebuilt/etc/apns-conf.xml:system/etc/apns-conf.xml \
 	$(VENDOR_PATH)/prebuilt/etc/apns-conf-cdma.xml:system/etc/apns-conf-cdma.xml \
 	$(VENDOR_PATH)/prebuilt/etc/spn-conf.xml:system/etc/spn-conf.xml \
+
+# Use prebuilt su until fixed when built
+PRODUCT_COPY_FILES += \
+        vendor/tg/prebuilt/common/xbin/su:system/xbin/su \
+        vendor/tg/prebuilt/app/SuperSU.apk:system/app/SuperSU.apk
